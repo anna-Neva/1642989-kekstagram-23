c6e550effa8a7b2a19fd1ca62e440b267b386d48
Your Name <74408496+anna-Neva@users.noreply.github.com>
2021-06-07 15:21:24 +0100

    Выполнение заданий - нужно больше функций (с исправлением)

--- js/main.js
+++ js/main.js
@@ -11,14 +11,3 @@ function checkString(text, maxLength) {
 };
 
 console.log(checkString('anna', 140));
-
-function randomInteger(min, max) {
-  Let rand;
-  if (min < 0 || max < 0) {
-    alert (‘Числа должны быть больше нуля’);
-  } else if (min > max) {
-    rand = max + Math.random() * (min + 1 – max);
-  } else {
-    rand = max + Math.random() * (max + 1 – min);
-    return Math.flor(rand);
-
